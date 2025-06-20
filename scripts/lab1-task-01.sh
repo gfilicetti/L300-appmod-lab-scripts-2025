ba1d654fa8441069e410b58a9b71ab2411f72a42
Gino Filicetti <ginof@google.com>
2025-06-20 12:05:23 -0400

    removed more hallucination

--- scripts/lab1-task-01.sh
+++ scripts/lab1-task-01.sh
@@ -7,9 +7,6 @@ REGION=us-central1
 # create a configuration for cloud workstations instance
 gcloud workstations configs create cepf-workstation-config \
     --region=$REGION \
-    --idle-timeout=30m \
-    --running-timeout=12h \
     --machine-type=e2-standard-4 \
-    --container-registry-access-token-type=none \
     --enable-nested-virtualization
 
