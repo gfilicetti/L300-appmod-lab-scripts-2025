3cd3df9dff77f3e4ddf0b43388687fcf89ef5bca
Gino Filicetti <ginof@google.com>
2025-06-20 12:08:07 -0400

    added cluster to task 1 lab 1

--- scripts/lab1-task-01.sh
+++ scripts/lab1-task-01.sh
@@ -3,9 +3,11 @@
 # variables
 PROJECT_ID=$(gcloud config get-value project)
 REGION=us-central1
+CLUSTER=cepf-workstation-cluster
 
 # create a configuration for cloud workstations instance
 gcloud workstations configs create cepf-workstation-config \
+    --cluster=$CLUSTER \
     --region=$REGION \
     --machine-type=e2-standard-4 \
     --enable-nested-virtualization
