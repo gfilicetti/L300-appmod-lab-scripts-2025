1866d0c59976e4ee0880929de0497c35917d91b1
Gino Filicetti <ginof@google.com>
2025-06-20 11:11:43 -0400

    first pass lab 1 task 1

---
+++ scripts/lab1-task-01.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# variables
+PROJECT_ID=$(gcloud config get-value project)
+CLUSTER=ws-cluster-01
+REGION=us-central1
+NETWORK=default
+SUBNETWORK=default
+
+# create a workstation cluster
+gcloud workstations clusters create $CLUSTER \
+    --region=$REGION \
+    --network=projects/$PROJECT_ID/global/networks/$NETWORK \
+    --subnetwork=projects/$PROJECT_ID/regions/$REGION/subnetworks/$SUBNETWORK
