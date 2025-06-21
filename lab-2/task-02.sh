a02f8d453b9b7a8ab40521e1ef64cba37d960713
Gino Filicetti <ginof@google.com>
2025-06-20 22:59:17 -0400

    fixed the sink syntax

--- lab-2/task-02.sh
+++ lab-2/task-02.sh
@@ -9,8 +9,8 @@ LOG_SINK_NAME="cepf_log_routing_sink"
 # 1. Create the Log Sink with Inclusion Filter
 echo "Creating Log Sink: $LOG_SINK_NAME"
 gcloud logging sinks create $LOG_SINK_NAME \
+    storage.googleapis.com/$LOG_BUCKET_NAME \
     --log-filter='resource.type="k8s_container"' \
-    --destination="storage.googleapis.com/projects/$PROJECT_ID/buckets/$LOG_BUCKET_NAME" \
     --project=$PROJECT_ID
 
 echo "Log Sink '$LOG_SINK_NAME' created to route k8s_container logs to bucket '$LOG_BUCKET_NAME'."
