2487a93349dac930a86a84f78adc2d6b9a8649f9
Gino Filicetti <ginof@google.com>
2025-06-20 22:53:35 -0400

    using fixed sink create command

--- lab-2/task-02.sh
+++ lab-2/task-02.sh
@@ -10,7 +10,7 @@ LOG_SINK_NAME="cepf_log_routing_sink"
 echo "Creating Log Sink: $LOG_SINK_NAME"
 gcloud logging sinks create $LOG_SINK_NAME \
     --log-filter='resource.type="k8s_container"' \
-    --destination="logging.googleapis.com/projects/$PROJECT_ID/locations/global/buckets/$LOG_BUCKET_NAME" \
+    --destination="storage.googleapis.com/projects/$PROJECT_ID/buckets/$LOG_BUCKET_NAME" \
     --project=$PROJECT_ID
 
 echo "Log Sink '$LOG_SINK_NAME' created to route k8s_container logs to bucket '$LOG_BUCKET_NAME'."
