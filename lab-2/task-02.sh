863fc2617e1f27c57cd9189cd8f54a96503190cb
Gino Filicetti <ginof@google.com>
2025-06-20 23:02:18 -0400

    granting perms as requested

--- lab-2/task-02.sh
+++ lab-2/task-02.sh
@@ -14,3 +14,10 @@ gcloud logging sinks create $LOG_SINK_NAME \
     --project=$PROJECT_ID
 
 echo "Log Sink '$LOG_SINK_NAME' created to route k8s_container logs to bucket '$LOG_BUCKET_NAME'."
+
+# 2. Grant permissions
+echo "Granting Storage Object Creator to the Logging Service Account"
+gcloud storage buckets add-iam-policy-binding gs://$LOG_BUCKET_NAME \
+    --member=serviceAccount:service-14723777617@gcp-sa-logging.iam.gserviceaccount.com \
+    --role=roles/storage.objectCreator
+
