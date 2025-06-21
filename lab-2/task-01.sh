ee15ac1cb96f78cde52c866e75855b664350707d
Gino Filicetti <ginof@google.com>
2025-06-20 20:16:11 -0400

    fixed region

--- lab-2/task-01.sh
+++ lab-2/task-01.sh
@@ -2,6 +2,7 @@
 
 # Variables
 PROJECT_ID=$(gcloud config get-value project)
+REGION=us-central1
 LOG_BUCKET_NAME="cepf_log_bucket"
 BQ_DATASET_NAME="cepf_dataset"
 
@@ -11,6 +12,6 @@ gcloud logging buckets create --location=global $LOG_BUCKET_NAME --project=$PROJ
 
 # 2. Create the BigQuery Dataset linked to the Log Bucket
 echo "Creating BigQuery Dataset: $BQ_DATASET_NAME"
-bq mk --location=global --dataset $PROJECT_ID:$BQ_DATASET_NAME
+bq mk --location=$REGION --dataset $PROJECT_ID:$BQ_DATASET_NAME
 
 echo "Log Bucket '$LOG_BUCKET_NAME' created and BigQuery Dataset '$BQ_DATASET_NAME' created."
