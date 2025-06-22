abdad9388f736aee977f9d72d3aaf96f35e321e7
Gino Filicetti <ginof@google.com>
2025-06-21 23:51:03 -0400

    Finally got task 1 for lab 2 correct

--- lab-2/task-01.sh
+++ lab-2/task-01.sh
@@ -5,11 +5,11 @@ PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUM=$(gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)")
 REGION=us-east4
 LOG_BUCKET_NAME="cepf_log_bucket"
-LOG_LINK_NAME="cepf_log_link"
 BQ_DATASET_NAME="cepf_dataset"
 SINK_NAME="${LOG_BUCKET_NAME}_to_${BQ_DATASET_NAME}_sink"
 
 # 1. Create the Log Bucket
+# make sure to enable analytics so we can connect it to BQ
 echo "Creating Log Bucket: $LOG_BUCKET_NAME"
 gcloud logging buckets create "$LOG_BUCKET_NAME" \
     --location=global \
@@ -17,15 +17,11 @@ gcloud logging buckets create "$LOG_BUCKET_NAME" \
     --project="$PROJECT_ID" || { echo "ERROR: Failed to create log bucket. Exiting."; exit 1; }
 
 # 2. Create the BigQuery Dataset
-echo "Creating BigQuery Dataset: $BQ_DATASET_NAME"
-bq --location="$REGION" mk --dataset "$PROJECT_ID:$BQ_DATASET_NAME" || { echo "ERROR: Failed to create BigQuery dataset. Exiting."; exit 1; }
-
-# 3. Now link the two together
-gcloud logging links create $LOG_LINK_NAME \
-  --location=$REGION \
+echo "Creating the linked BigQuery Dataset: $BQ_DATASET_NAME"
+gcloud logging links create $BQ_DATASET_NAME \
+  --location=global \
   --project=$PROJECT_ID \
-  --bucket=$LOG_BUCKET_NAME \
-  --dataset=$BQ_DATASET_NAME || { echo "ERROR: Failed to link Log Bucket and BigQuery dataset. Exiting."; exit 1; }
+  --bucket=$LOG_BUCKET_NAME || { echo "ERROR: Failed to create the linked BigQuery dataset. Exiting."; exit 1; }
 
 echo "Log Bucket '$LOG_BUCKET_NAME' and BigQuery Dataset '$BQ_DATASET_NAME' created and linked."
 
