4ecb05e05e9979714fa0b89976238fb9fe3a14a5
Gino Filicetti <ginof@google.com>
2025-06-26 18:32:41 -0400

    zone change

--- lab-4/task-02.sh
+++ lab-4/task-02.sh
@@ -8,9 +8,9 @@
 PROJECT_ID=$(gcloud config get-value project)
 # NOTE: Qwiklabs will give you a region to use in the instructions once the environment is provisioned. Use that region here.
 REGION1="us-west1" # Lab start region / location of the first cluster
-ZONE1="$REGION1-c" # the zone for gke cluster 1
+ZONE1="$REGION1-a" # the zone for gke cluster 1
 REGION2="europe-west4"    # Location of the second cluster
-ZONE2="$REGION2-b" # the zone for gke cluster 2
+ZONE2="$REGION2-a" # the zone for gke cluster 2
 
 # NOTE: Update with the names of the pre-existing GKE clusters
 CLUSTER1_NAME="cepf-gke-cluster-1"
